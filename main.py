75df646d77b10fbe444d31cd412cf4acae474e1d
jkusa <jkilroy@gmail.com>
2025-12-07 12:53:10 -0600

    Add connected message

--- main.py
+++ main.py
@@ -8,25 +8,34 @@ PORT = "8080"
 keyboard = Controller()
 pygame.init()
 pygame.joystick.init()
+
 joystick_count = pygame.joystick.get_count()
 if joystick_count > 0:
+
     joystick = pygame.joystick.Joystick(0)
     joystick.init() 
+    print(f"Connected to {joystick.get_name()}")
+
     running = True
     while running:
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 running = False
-            
+
             if event.type == pygame.JOYBUTTONDOWN:
                 if event.button == 1:
                     requests.get(f"http://{IP}:{PORT}/koreader/event/GotoViewRel/-1")
+
                 elif event.button == 2:
                     requests.get(f"http://{IP}:{PORT}/koreader/event/GotoViewRel/1")
+
                 elif event.button == 17:
                     requests.get(f"http://{IP}:{PORT}/koreader/event/IncreaseFlIntensity/1")
+
                 elif event.button == 19:
                     requests.get(f"http://{IP}:{PORT}/koreader/event/DecreaseFlIntensity/1")
+
                 else:
                     print(event.button)
 else:
